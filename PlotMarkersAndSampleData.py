2f0309a95e6704f3e38b553ec3e130b696c47d47
sibisrinath@gmail.com <srinathsibi>
2019-09-27 05:44:39 -0400

    The process to convert the all of the data into readable data takes too much memory and the process gets killed. Need to find a way to write out the relevant data without all the nonsense.

--- PlotMarkersAndSampleData.py
+++ PlotMarkersAndSampleData.py
@@ -1,6 +1,23 @@
 #Author: Srinath Sibi, Email: ssibi@stanford.edu
-#Purpose: To plot the markers and see how we can change the event markers in order to restore the
-import glob, os, sys, shutil,re
+#Purpose: To plot the markers and see how we can change the event markers to something sane
+#Overall Important columns:
+# Time : Use column 10 and split and convert the time into seconds using the iMotionsTimeConverter converter. Also compare to TImestamp CAL (mSecs) in column 20
+# Details: 1) UTC TImestamp -> Col 10 ; Timestamp -> Col 20
+# Markers are different from sim markers.
+# Details : 1) SimulatorEvent (0,0) -> Col 15
+#First, we read and convert the input the columns that are of interest to us. We store them alongside the time and the markers in each file
+#The following are the files that need to be made:
+# 1. HR
+# 2. GSR
+# 3. PPG
+# 4. Drive Data
+#First the HR file.
+# ECG Details: 1) The ECG LL-RA CAL (mVolts) (ShimmerECG) data , ECG LL-RA RAW (no units) (ShimmerECG) and the ECG LA-RA CAL (mVolts) (ShimmerECG) columns work well.
+# The corresponding columns are AS(col 45), AT(col 46), AU( col 47).
+# Drive Details: 1) Brake -> Col 14 ; SimulatorEvent -> Col 15 ; Speed -> 16 ; Steer -> 17 ; Throttle -> 18
+# PPG Details : 1) PPG heart beat-> Col 28 ; 2) PPG filtered pre-HR signal-> Col 25
+# GSR Details : 1) GSR CAL (micro siemens)-> Col 30
+import os, sys, csv , re
 import matplotlib as plt
 import numpy as np
 import csv
@@ -10,8 +27,85 @@ from scipy import interpolate
 import numpy as np
 import pandas as pd
 from statistics import mean
-LOGFILE = os.path.abspath('.') + '/CurrentOutputFile.csv'
+LOGFILE = os.path.abspath('.') + '/CurrentOutputFile.csv'#Using one output file for all the scripts from now to avoid flooding the working folder
 MAINPATH = os.path.abspath('.')#Always specify absolute path for all path specification and file specifications
-LISTOFPARTICIPANTS =[]#This is the list of all participants in the Data folder
-LISTOFSECTIONS =[]# List of all sections for a participant
 DEBUG = 0# To print statements for debugging
+TOPELIMINATION = 50
+#We use this function as a key to sorting the list of folders (participants)
+def SortFunc(e):
+    return int( re.sub('P','',e) )
+#the time converter from iMotions data. Use the column Timestamp (col 10) from the iMotions file.
+def iMotionsTimeConverter(inputcode):
+    decimal = float(inputcode%1000)/1000
+    inputcode = (inputcode - 1000*decimal)/1000
+    secs = float(inputcode%100)
+    inputcode = (inputcode -secs)/100
+    mins = float(inputcode%100)
+    inputcode = (inputcode-mins)/100
+    hours = float(inputcode)
+    time_abs = hours*3600 + mins*60 + secs + decimal
+    return time_abs
+#Main Function
+if __name__ == '__main__':
+    #Opening the Logging file
+    file = open(LOGFILE , 'wb')
+    writer = csv.writer(file)
+    writer.writerow(['The output for the Plotting and Sampling the Data.'])
+    file.close()
+    try:
+        #print " The Main Function for the Plotting and Sampling Function"
+        listoffolders = os.listdir(MAINPATH+'/Data/')
+        #Sort the Folders
+        listoffolders.sort(key=SortFunc)
+        if DEBUG == 1:
+            print "\nThe list of folder:", listoffolders
+        for participant in listoffolders:
+            #path to the contents of the folder.
+            folderpath = MAINPATH+'/Data/'+participant+'/'
+            #opening the iMotions File in each folder
+            try:
+                file = open(folderpath+ participant + '.txt','r')
+                reader = csv.reader(file)
+                #print "First and Second lines in file: " , next(reader)
+                #We need to take the first 6 lines of the data out to record in a new info file, should we need it later.
+                InfoList =[]
+                for i in range(6):
+                    if i==5:
+                        row = next(reader)[0]
+                        headerrow = row.split('\t')
+                        InfoList.append(headerrow)
+                    elif i<5:
+                        InfoList.append(next(reader))
+                #If the process worked, the last row should be the header column
+                if DEBUG == 1:
+                    print "\n\n\nInfoList :\n" , InfoList
+                data = list(reader)#Super List of lists containing all the data in one location.
+                file.close()
+                #Write the info file
+                file = open(folderpath+participant+'iMotionsInfo.txt','wb')
+                writer = csv.writer(file)
+                writer.writerows(InfoList)
+                file.close()
+                #To ensure that there are no empty cells in the data uber-list , we delete the first 30 rows.
+                for i in range(TOPELIMINATION):
+                    data.remove( data[0] )#We always remove the first element
+                #Now to make sure that the rows are properly divided
+                re_data = []
+                for row in data:
+                    re_data.append( row[0].split('\t') )
+                if DEBUG == 0:
+                    print "\n\n\nFirst line of Reloaded Data : " , re_data[0]
+            except Exception as e:
+                print " Exception recorded for participant : ", participant
+                print 'Error on line {}'.format(sys.exc_info()[-1].tb_lineno)
+                file = open(LOGFILE, 'a')
+                writer = csv.writer(file)
+                writer.writerow([' Exception for participant ', e , 'on line {}'.format(sys.exc_info()[-1].tb_lineno) ])
+                file.close()
+    except Exception as e:
+        print "Main Exception Catcher" ,e
+        print 'Error on line {}'.format(sys.exc_info()[-1].tb_lineno)
+        file = open(LOGFILE,'a')
+        writer = csv.writer(file)
+        writer.writerow([' Main Function Exception Catcher ',e,'Error on line {}'.format(sys.exc_info()[-1].tb_lineno) ])
+        file.close()
---
+++ PlottingFunctions.py
@@ -0,0 +1,76 @@
+#This function is used to plot the data:
+#1. plotdata : 2 dimensional list of data that needs to be plotted
+#2. xlabel and ylabel : axes labels for x and y axes , type : strings
+#3. plottitle : The title of the plot at the top center of the plot, type : string
+#4. savename : The name of the plot when saved, type : string, note: use pdf extension
+#5. LOGFILE : Use the LOGFILE with absolute path, type : string
+#6. participant : Participant folder, type: string
+#7. section : section folder name , type : string
+#8. savepath : the absolute location of the saved plot, type : string
+import glob, os, sys, shutil
+import matplotlib as plt
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
+from scipy import interpolate
+import numpy as np
+plt.rcParams.update({'font.size': 3.5})
+DEBUG = 0#Variable to identify what if anything is wrong with the PERCLOS calcuator
+def Plot2Data( plotdata, ylabel, plottitle, savename, LOGFILE, participant, section, savepath, xlabel = 'Time (in seconds)' ):
+    if DEBUG == 1:
+        print "Plotting function called for : ", ylabel
+    try:
+        try:
+            x_data = [ float(plotdata[i][0]) for i in range(len(plotdata)) ]
+            y_data = [ float(plotdata[i][1]) for i in range(len(plotdata)) ]
+        except:
+            x_data = [ float(plotdata[i][0]) for i in range(len(plotdata)) ]
+            y_data = [ str(plotdata[i][1]) for i in range(len(plotdata)) ]
+        #starting the plot
+        fig = plt.figure()
+        fig.tight_layout()
+        plt.title(plottitle)
+        plt.plot(x_data, y_data, 'r-', label = ylabel )
+        if DEBUG == 1:
+            print "First few elements of the x and y data are : ", x_data[0:3] ,'\n', y_data[0:3]
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.legend(loc = 'upper right')
+        plt.savefig(savepath + savename, bbox_inches = 'tight', dpi=900 , quality = 100)
+        plt.close()
+    except Exception as e:
+        print "Exception at the plotting function in PlottingFunctions.py : ", e
+        file = open(LOGFILE, 'a')
+        writer = csv.writer(file)
+        writer.writerow([' Exception in the plotting function ',  ' Participant: ' , participant , ' Section : ', section , '  ' , ' Exception: ', e])
+        file.close()
+def Plot3Data(x_data , y_data , z_data , ylabel , zlabel , plottitle , savename, LOGFILE, participant, section , savepath , verticallineindices=[0] , grid = 1, xlabel = 'Time (in Seconds)'):
+    if DEBUG == 1:
+        print "Plotting function called for : ", ylabel
+    try:
+        #starting the plot
+        fig = plt.figure()
+        fig.tight_layout()
+        plt.title(plottitle)
+        plt.plot(x_data, y_data, 'r-', label = ylabel)
+        plt.plot(x_data, z_data, 'g--', label = zlabel)
+        if DEBUG == 1:
+            print "First few elements of the x,y and z data are : ", x_data[0:3] ,'\n', y_data[0:3] , '\n', z_data[0:3]
+        if len(verticallineindices) > 1:#Meaning the verticallineindices array is not empty
+            for i in range(len(verticallineindices)):
+                if verticallineindices[i]==1:
+                    plt.axvline(x = x_data[i], linewidth = '1')
+        plt.xlabel(xlabel)
+        plt.ylabel(str(ylabel) + ' and ' + str(zlabel))
+        plt.legend(loc = 'upper right')
+        if grid == 1:
+            plt.grid(color = 'b' , linestyle = '-.', linewidth = 0.1 )
+        #plt.show()
+        plt.savefig(savepath + savename, bbox_inches = 'tight', dpi=900 , quality=100)
+        plt.close()
+    except Exception as e:
+        print "Exception at the plotting function in PlottingFunctions.py : ", e
+        file = open(LOGFILE, 'a')
+        writer = csv.writer(file)
+        writer.writerow([' Exception in the plotting function ',  ' Participant: ' , participant , ' Section : ', section , '  ' , ' Exception: ', e])
+        file.close()
