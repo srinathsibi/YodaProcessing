2e9e6a9dd20d1652c021170f15e90a8587356129
sibisrinath@gmail.com <srinathsibi>
2019-09-29 07:51:10 -0400

    Process currently crashes when attempting to store all the respective variables in a single array. To avoid this, we are going to break it up and write into files rather than store them. The plots will be generated from reading the files rather than plotting from stored lists.

--- PlotMarkersAndSampleData.py
+++ PlotMarkersAndSampleData.py
@@ -12,10 +12,12 @@
 # 3. PPG
 # 4. Drive Data
 #First the HR file.
-# ECG Details: 1) The ECG LL-RA CAL (mVolts) (ShimmerECG) data , ECG LL-RA RAW (no units) (ShimmerECG) and the ECG LA-RA CAL (mVolts) (ShimmerECG) columns work well.
-# The corresponding columns are AS(col 45), AT(col 46), AU( col 47).
-# Drive Details: 1) Brake -> Col 14 ; SimulatorEvent -> Col 15 ; Speed -> 16 ; Steer -> 17 ; Throttle -> 18
-# PPG Details : 1) PPG heart beat-> Col 28 ; 2) PPG filtered pre-HR signal-> Col 25
+# ECG Details: 1) For CAT 1: The ECG LL-RA CAL (mVolts) (ShimmerECG) data , ECG LL-RA RAW (no units) (ShimmerECG) and the ECG LA-RA CAL (mVolts) (ShimmerECG) columns work well.
+# The corresponding columns are cols 45, 46 , 47 , 48.
+# 2) For CAT 2: The ECG values are under the first two columns mislabelled as EMG and are present in Columns 37 , 38 , 39 , 40.
+# 3) For CAT3 (participant 12): The ECG values are in the columns 32 , 33 , 34 , 35
+# Drive Details: 1) Brake -> Col 14 ; Speed -> 16 ; Steer -> 17 ; Throttle -> 18
+# PPG Details : 1) PPG heart beat-> Col 28 ;
 # GSR Details : 1) GSR CAL (micro siemens)-> Col 30
 import os, sys, csv , re
 import matplotlib as plt
@@ -31,7 +33,12 @@ LOGFILE = os.path.abspath('.') + '/CurrentOutputFile.csv'#Using one output file 
 MAINPATH = os.path.abspath('.')#Always specify absolute path for all path specification and file specifications
 DEBUG = 0# To print statements for debugging
 TOPELIMINATION = 50
-#We use this function as a key to sorting the list of folders (participants)
+#The 3 categories are defined here. Check here before adding the
+#We use this function as a key to sorting the list of folders (participants).
+CAT1 = [ 'P002', 'P004' , 'P005' , 'P007' , 'P008' , 'P009' , 'P010' , 'P011' , 'P013' , 'P016' , 'P021' , 'P023' , 'P024' , 'P025' , 'P028' , 'P032' ]
+CAT2 = [ 'P022' , 'P037' , 'P040' , 'P042' , 'P045' , 'P046' , 'P047' , 'P055' , 'P057' , 'P058' , 'P060' , 'P064' , 'P066' , 'P067' , 'P069' , 'P071' , \
+'P076' , 'P077' , 'P085' , 'P086' , 'P087' , 'P088' , 'P089' , 'P090' , 'P090' , 'P093' , 'P094' , 'P095' , 'P096' , 'P097' ,'P098']
+CAT3 = ['P012']
 def SortFunc(e):
     return int( re.sub('P','',e) )
 #the time converter from iMotions data. Use the column Timestamp (col 10) from the iMotions file.
@@ -89,18 +96,31 @@ if __name__ == '__main__':
                 #To ensure that there are no empty cells in the data uber-list , we delete the first 30 rows.
                 for i in range(TOPELIMINATION):
                     data.remove( data[0] )#We always remove the first element
+                if DEBUG==0 and participant in CAT1:    print "Participant in CAT1 , " , participant
+                if DEBUG==0 and participant in CAT2:    print "Participant in CAT2 , " , participant
+                if DEBUG==0 and participant in CAT3:    print "Participant in CAT3 , " , participant
                 #Now to make sure that the rows are properly divided
                 re_data = []
+                #Adding a header row to
                 for row in data:
-                    re_data.append( row[0].split('\t') )
+                    #Order of data : <TImestamp 1 , TImestamp 2 , Markers , ECG , Brake , Speed , Steer , Throttle , PPG , PPG_RAW , GSR>
+                    if participant in CAT1:
+                        re_data.append( [ row[0].split('\t')[9] , row[0].split('\t')[19] , row[0].split('\t')[14] , row[0].split('\t')[44] , row[0].split('\t')[45] , row[0].split('\t')[46] , row[0].split('\t')[47] , row[0].split('\t')[13] \
+                        , row[0].split('\t')[15] , row[0].split('\t')[16] , row[0].split('\t')[17] , row[0].split('\t')[27] , row[0].split('\t')[29] ] )
+                    elif participant in CAT2:
+                        re_data.append( [ row[0].split('\t')[9] , row[0].split('\t')[19] , row[0].split('\t')[14] , row[0].split('\t')[36] , row[0].split('\t')[37] , row[0].split('\t')[38] , row[0].split('\t')[39] , row[0].split('\t')[13] \
+                        , row[0].split('\t')[15] , row[0].split('\t')[16] , row[0].split('\t')[17] , row[0].split('\t')[27] , row[0].split('\t')[29] ] )
+                    elif participant in CAT3:
+                        re_data.append( [ row[0].split('\t')[9] , row[0].split('\t')[19] , row[0].split('\t')[14] , row[0].split('\t')[31] , row[0].split('\t')[32] , row[0].split('\t')[33] , row[0].split('\t')[34] , row[0].split('\t')[13] \
+                        , row[0].split('\t')[15] , row[0].split('\t')[16] , row[0].split('\t')[17] , row[0].split('\t')[27] , row[0].split('\t')[29] ] )
                 if DEBUG == 0:
-                    print "\n\n\nFirst line of Reloaded Data : " , re_data[0]
+                    print "\n\n\nParticipant: " , participant , "\nFirst line of Reloaded Data : " , re_data[10000]
             except Exception as e:
                 print " Exception recorded for participant : ", participant
                 print 'Error on line {}'.format(sys.exc_info()[-1].tb_lineno)
                 file = open(LOGFILE, 'a')
                 writer = csv.writer(file)
-                writer.writerow([' Exception for participant ', e , 'on line {}'.format(sys.exc_info()[-1].tb_lineno) ])
+                writer.writerow([' Exception for participant ', participant , 'Exception : ', e , 'on line {}'.format(sys.exc_info()[-1].tb_lineno)])
                 file.close()
     except Exception as e:
         print "Main Exception Catcher" ,e
